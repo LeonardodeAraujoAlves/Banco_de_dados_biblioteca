67e81406f9564e138354442f7696805476298049
LeonardodeAraujoAlves <78648608+LeonardodeAraujoAlves@users.noreply.github.com>
2022-12-03 15:49:04 -0300

    Update biblioteca.sql
    remoção dos inserts

--- biblioteca.sql
+++ biblioteca.sql
@@ -45,15 +45,3 @@ alter TABLE Emprestimo add CONSTRAINT fk_cod_localizacao FOREIGN key(fk_cod_loca
 
 alter table Aluno add constraint fk_id_livroAlugado foreign key(livros_alugados) references Livro(cod_livro);
 
-
-/*
-INSERT into Categoria_livro values(92,"Não didatico");
-insert into Localizaco_livro values(201,"Terror");
-insert into Livro values(22,"Livrado do livro","Zé do picadinho",4,92,201);
-insert into Aluno values(01,"Jiban",1,'Cursando');
-INSERT into Emprestimo values(01,22,01,"Livrado do livro",92,201);
-SELECT * from Categoria_livro;
-SELECT * from Aluno;
-SELECT * from Livro;
-SELECT * from Emprestimo, Aluno WHERE Emprestimo.fk_aluno_ra = Aluno.ra;
-*/
